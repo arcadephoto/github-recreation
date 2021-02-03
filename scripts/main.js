6f8d9b9a4bf8c035764fd2d10e6852c6bbafaea6
arcadephoto <75449898+arcadephoto@users.noreply.github.com>
2021-02-02 23:14:09 -0500

    working on code to change colored dots

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,37 @@
-(function() {
 
-  'use strict';
 
 const BASE_URL = 'https://api.github.com/users/arcadephoto';
 
-// let profileData = {
-//
-// };
+//the following functions work. they take the data from fetch then store
+// it as a variable called testData. Then the sortTest function takes the "updated_at"
+// field from the array, then sorts it in reverse chronological order. I don't know
+// what to do with it from there.
 
 
+let datearray = [];
+let testData;
 
 
+function sortTest(){
+  for(let i = 0; i < testData.repos.length; i++){
+    datearray.push(testData.repos[i].updated_at);
+  }
+}
 
 
 
 
 
 const generateHTML = (data) => {
-  // console.log('data', data);
+  // console.log(data);
   const source = document.getElementById("gitInformation").innerHTML;
   const template = Handlebars.compile(source);
   const context = data;
   const html = template(context);
   // console.log("html", html);
   document.querySelector('.bio').innerHTML = html;
+//extract usable informaton field from array
+//  console.log(data.name);
   // profileData.name = data.name;
 }
 
@@ -39,7 +47,7 @@ const getOrg = (data) => {
 }
 
 const repos = (data) => {
-  // console.log(data);
+  console.log(data);
   const source = document.getElementById("repos").innerHTML;
   const template = Handlebars.compile(source);
   const context = data;
@@ -47,6 +55,7 @@ const repos = (data) => {
   // console.log("html", html);
   document.querySelector('.repos').innerHTML = html;
   // profileData.name = data.name;
+  testData = data;
 }
 
 
@@ -70,11 +79,9 @@ fetch(`${BASE_URL}/repos`)
 .then(response => response.json())
 .then(data => repos({repos:data}));
 
-
+//trying to use Moment js to convert the date field from Github to
+//"<hours> hours ago" format
 const timeStuff = "test string";
 // document.getElementById('timeMoment').innerHTML = timeStuff;
 const getTime = moment().startOf('day').fromNow();
 console.log(getTime);
-
-
-})();
